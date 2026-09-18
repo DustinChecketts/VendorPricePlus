6eacf1960634e961317db9037624420d488800c0
Dustin Checketts <8584623+DustinChecketts@users.noreply.github.com>
2026-09-18 16:02:10 -0400

    Refactor client compatibility layer

--- Compat.lua
+++ Compat.lua
@@ -1,62 +1,35 @@
 local VP = VendorPricePlus
+VP.Compat = VP.Compat or {}
 
-local function SetPrice(tt, count, item)
-	VP:SetPrice(tt, false, "Compat", count, item, true)
+local Compat = VP.Compat
+
+function Compat.GetItemInfo(item)
+    if C_Item and C_Item.GetItemInfo then
+        return C_Item.GetItemInfo(item)
+    end
+    if GetItemInfo then
+        return GetItemInfo(item)
+    end
 end
 
-function VP:IsShown(frame)
-	return frame and frame:IsVisible() and frame:IsMouseOver()
+function Compat.GetContainerItemInfo(bag, slot)
+    if C_Container and C_Container.GetContainerItemInfo then
+        return C_Container.GetContainerItemInfo(bag, slot)
+    end
+    if GetContainerItemInfo then
+        local _, count = GetContainerItemInfo(bag, slot)
+        if count then
+            return { stackCount = count }
+        end
+    end
 end
 
-local Auctioneer = {
-	AucAdvAppraiserFrame = function(tt)
-		local itemID = select(2, tt:GetItem()):match("item:(%d+)")
-		for _, v in pairs(AucAdvAppraiserFrame.list) do
-			if v[1] == itemID then
-				SetPrice(tt, v[6])
-				break
-			end
-		end
-	end,
-	AucAdvSearchUiAuctionFrame = function(tt)
-		local row = tt:GetOwner():GetID()
-		local count = AucAdvanced.Modules.Util.SearchUI.Private.gui.sheet.rows[row][4]
-		SetPrice(tt, tonumber(count:GetText()))
-	end,
-	AucAdvSimpFrame = function(tt)
-		SetPrice(tt, AucAdvSimpFrame.detail[1])
-	end,
-}
-
-GameTooltip:HookScript("OnTooltipSetItem", function(tt)
-	if AucAdvanced and VP:IsShown(AuctionFrame) then
-		for frame, func in pairs(Auctioneer) do
-			if VP:IsShown(_G[frame]) then
-				func(tt)
-				break
-			end
-		end
-	elseif AuctionFaster and VP:IsShown(AuctionFrame) and AuctionFrame.selectedTab >= 4 then
-		local count
-		if AuctionFrame.selectedTab == 4 then -- sell
-			local item = tt:GetOwner().item
-			count = item and item.count
-		elseif AuctionFrame.selectedTab == 5 then -- buy
-			local hoverRowData = AuctionFaster.hoverRowData
-			count = hoverRowData and hoverRowData.count -- provided by AuctionFaster
-		end
-		SetPrice(tt, count)
-	elseif AtlasLoot and VP:IsShown(_G["AtlasLoot_GUI-Frame"]) then
-		SetPrice(tt)
-	else -- Chatter, Prat: check for active chat windows
-		local mouseFocus = tt:GetOwner()
-		if mouseFocus and mouseFocus.GetObjectType and mouseFocus:GetObjectType() == "FontString" then
-			for i = 1, FCF_GetNumActiveChatFrames() do
-				if _G["ChatFrame"..i]:IsMouseOver() then
-					SetPrice(tt)
-					break
-				end
-			end
-		end
-	end
-end)
+function Compat.IsAddOnLoaded(name)
+    if C_AddOns and C_AddOns.IsAddOnLoaded then
+        return C_AddOns.IsAddOnLoaded(name)
+    end
+    if IsAddOnLoaded then
+        return IsAddOnLoaded(name)
+    end
+    return false
+end
